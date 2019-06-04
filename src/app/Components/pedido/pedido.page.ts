efb713bcf9ff4751da187a7bc967ec35cf3fb097
Victor <elaburrimientomanda@gmail.com>
2019-06-04 07:51:33 +0200

    ca,bios

--- src/app/Components/pedido/pedido.page.ts
+++ src/app/Components/pedido/pedido.page.ts
@@ -1,4 +1,6 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { PedidosService } from 'src/app/services/pedidos.service';
 
 @Component({
   selector: 'app-pedido',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidoPage implements OnInit {
 
-  constructor() { }
+  Pedido;
+  listaEspecificaciones;
+
+  constructor(private pedidosService:PedidosService,private _route: ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
+    this.cargarPedido()
+  }
+  cargarPedido(){
+    this.pedidosService.getallByPedido(this._route.snapshot.paramMap.get('id')).subscribe(res =>{
+      this.Pedido = res[0];
+      console.log( res[0]);
+      this.pedidosService.getEspecificacionesByPedido(this.Pedido.codpedido).subscribe(res =>{
+        this.listaEspecificaciones=res;
+        console.log(res,"Especificaciones");
+      });
+      
+    });
+    
+  }
+  comprar(){
+    
+    this.pedidosService.update(this.Pedido,this.listaEspecificaciones);
   }
 
+  UpdateTotal(){
+    this.Pedido.total=0;
+    for (let i = 0; i < this.listaEspecificaciones.length; i++) {
+      this.listaEspecificaciones[i].precio =(this.listaEspecificaciones[i].cantidad*this.listaEspecificaciones[i].id.art.precio_art);
+      this.Pedido.total = this.Pedido.total + parseFloat(this.listaEspecificaciones[i].precio)
+    }
+  }
 }
